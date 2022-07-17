20431584b38244c88b2a13e672b74619cb734454
studiopasokon <andy@studiopasokon.com>
2022-07-17 21:15:27 +0200

    Add simple loop to read input and tokenize it

--- src/dot4thshell/Program.cs
+++ src/dot4thshell/Program.cs
@@ -1,3 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿Console.WriteLine("Welcome to dot4th, a simple Forth interpreter");
+Console.WriteLine();
 
-Console.WriteLine("Hello, World!");
+var running = true;
+while (running)
+{
+    Console.Write(">>> ");
+    var inputLine = Console.ReadLine();
+    
+    // TODO: replace this by a real processing of the Forth word 'bye'.
+    if (inputLine is "bye")
+        running = false;
+
+    // Process the input.
+    var tokens = inputLine?.Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+    if (tokens is not null)
+        foreach (var token in tokens)
+        {
+            Console.WriteLine(token);
+        }
+    
+    Console.WriteLine();
+}
